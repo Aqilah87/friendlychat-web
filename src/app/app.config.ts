98e7273501081925d45e6777dd82f499ea694d4f
Aqilah87 <nooraqilah087@gmail.com>
2024-12-30 09:53:57 +0800

    angluar update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-2dd42","appId":"1:372630238144:web:161aa2c2c026570fb50e25","storageBucket":"friendlychat-2dd42.firebasestorage.app","apiKey":"AIzaSyC3NzOrr_zvyL0Ow2kCjxLiZC5KOKi-2Ro","authDomain":"friendlychat-2dd42.firebaseapp.com","messagingSenderId":"372630238144"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
