ac5a88bb1ab8fb6340303c40390f8f7eaae6e318
Aqilah87 <nooraqilah087@gmail.com>
2025-01-13 10:03:40 +0800

    Add the ability to post images

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,3 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-2dd42","appId":"1:372630238144:web:161aa2c2c026570fb50e25","storageBucket":"friendlychat-2dd42.firebasestorage.app","apiKey":"AIzaSyC3NzOrr_zvyL0Ow2kCjxLiZC5KOKi-2Ro","authDomain":"friendlychat-2dd42.firebaseapp.com","messagingSenderId":"372630238144"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
+provideMessaging(() => {
+  return getMessaging();
+})
